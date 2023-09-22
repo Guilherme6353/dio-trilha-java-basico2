0cc31eb8b6dd7a4941e510a5515382588c03de27
Guilherme Henrique <ghn.cnc@gmail.com>
2023-09-22 09:46:17 -0300

    Fim

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,13 +4,13 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
         Scanner scan = new Scanner(System.in);
-        //TODO: Conhecer e importar a classe Scanner
+        // TODO: Conhecer e importar a classe Scanner
 
-        //Exibr as mensagens para o nosso usuário
-        
-        //Obter pelo Scanner os valores digitados pelo terminal
+        // Exibr as mensagens para o nosso usuário
 
-        //Exibir mensagem conta criada
+        // Obter pelo Scanner os valores digitados pelo terminal
+
+        // Exibir mensagem conta criada
 
         System.out.println("Por favor insira seu nome:");
         String nomeCliente = scan.nextLine();
@@ -24,6 +24,8 @@ public class ContaTerminal {
         System.out.println("Informe o seu saldo:");
         double saldo = scan.nextDouble();
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", Conta " + numeroConta + " e seu saldo é R$:" + saldo + "0" + " já está disponível para saque.");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", Conta " + numeroConta + " e seu saldo é R$:" + saldo + "0"
+                + " já está disponível para saque.");
     }
 }
